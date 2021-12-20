e67868976c955d0a4dfdffbbdd85c04e8a488f22
Stephen Mahon <stemahon@gmail.com>
2021-12-19 23:22:41 -0600

    simplified functions

--- Day3/binary.go
+++ Day3/binary.go
@@ -2,39 +2,33 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
-	"strings"
 )
 
 // add bits func back into oxygenscrubber for new version of gamma based on remaining vals.
 // rework gamma func to return consistent types for new functions. Maybe just a string of binary numbers and do the conversion in main()
 
 func main() {
-	fileName := flag.String("f", "test.txt", "input file name")
-	flag.Parse()
+	fileName := "input.txt"
 
-	vals, err := readData(*fileName)
+	vals, err := readData(fileName)
 	if err != nil {
-		log.Fatalf("could not read %s: %v", *fileName, err)
+		log.Fatalf("could not read %s: %v", fileName, err)
+	}
+	bits := make([][]string, len(vals[0]))
+	for i := range vals {
+		for j := range vals[i] {
+			bits[j] = append(bits[j], string(vals[i][j]))
+		}
 	}
 
-	gamma, err := bits(vals)
-	if err != nil {
-		log.Fatalf("could not convert vals to binary: %v", err)
-	}
+	gamma, _ := readGamma(bits)
+	epsilon, _ := readEpsilon(bits)
+	fmt.Println(gamma * epsilon)
 
-	epilson, err := binaryInvert(gamma)
-	if err != nil {
-		log.Fatalf("could not convert vals to binary: %v", err)
-	}
-
-	fmt.Printf("Part 1\ngamma: %v\nepilson: %v\nPower consumption: %v\n", gamma, epilson, gamma*epilson)
-
-	fmt.Println(oxygenRating(vals, gamma))
 }
 
 func readData(path string) (vals []string, err error) {
@@ -60,76 +54,36 @@ func readData(path string) (vals []string, err error) {
 	return vals, nil
 }
 
-func bits(vals []string) (int, error) {
-	var sigB string
-	for i := 0; i < len(vals[0]); i++ {
-		bin := ""
-		for _, val := range vals {
-			bin += string(val[i])
-		}
-		if condition(bin) {
-			sigB += "1"
+func cannonize(vals []string) map[string]int {
+	dict := make(map[string]int)
+	for _, v := range vals {
+		dict[v] = dict[v] + 1
+	}
+	return dict
+}
+
+func readGamma(bits [][]string) (int64, error) {
+	var bit string
+	for i := range bits {
+		dict := cannonize(bits[i])
+		if dict["0"] > dict["1"] {
+			bit += "0"
 		} else {
-			sigB += "0"
+			bit += "1"
 		}
 	}
-
-	output, err := strconv.ParseInt(sigB, 2, 64)
-	if err != nil {
-		fmt.Println(err)
-		return -1, err
-	}
-	return int(output), nil
+	return strconv.ParseInt(bit, 2, 64)
 }
 
-func condition(bin string) bool {
-	return strings.Count(bin, "1") >= strings.Count(bin, "0")
-}
-
-func binaryInvert(input int) (int, error) {
-	val := strconv.FormatInt(int64(input), 2)
-	var invBin string
-	for _, v := range val {
-		if string(v) == "0" {
-			invBin += "1"
+func readEpsilon(bits [][]string) (int64, error) {
+	var bit string
+	for i := range bits {
+		dict := cannonize(bits[i])
+		if dict["1"] > dict["0"] {
+			bit += "0"
 		} else {
-			invBin += "0"
+			bit += "1"
 		}
 	}
-	output, err := strconv.ParseInt(invBin, 2, 64)
-	if err != nil {
-		fmt.Println(err)
-		return -1, err
-	}
-	return int(output), nil
-}
-
-func oxygenRating(vals []string, gammaInt int) (int, error) {
-
-	gamma := strconv.FormatInt(int64(gammaInt), 2)
-
-	for i := range gamma {
-		tmp := []string{}
-		for j := range vals {
-			if gamma[i] == vals[j][i] {
-				tmp = append(tmp, vals[j])
-			}
-		}
-		vals = tmp
-		if len(vals) == 1 {
-			break
-		}
-		gamma, _ = bits(vals)
-		if err != nil {
-			log.Fatalf("could not convert vals to binary: %v", err)
-		}
-	}
-	fmt.Println(vals)
-	output, err := strconv.ParseInt(vals[0], 2, 64)
-	if err != nil {
-		fmt.Println(err)
-		return -1, err
-	}
-	return int(output), nil
-
+	return strconv.ParseInt(bit, 2, 64)
 }
