f60290ddb1c11b9c5e0a2c536d4a73d059668885
Stephen Mahon <stemahon@gmail.com>
2023-01-03 16:03:18 -0600

    slight bug - sorting only half the array

--- 2022/1/js/sketch.js
+++ 2022/1/js/sketch.js
@@ -1,41 +1,65 @@
 let elves = [];
 let input;
-let index = 0;
-
 let binWidth;
+
 let maxCal = 0;
+let n = 0;
 
 function preload() {
-  input = loadStrings("test.txt");
+  input = loadStrings("input.txt");
 }
+
 function setup() {
   createCanvas(400, 400);
-  background(155);
 
-  let calories = [];
+  let calories = [];  
+// sorting only half the array!
   for (let i = 0; i < input.length; i++) {
     let value = parseFloat(input[i]);
 
     if (value) {
-      calories.push(value)
+      calories.push(value);
     } else {
-      elf = new Bar(index, calories);
-      elves.push(elf);
+      elves.push(total(calories));
+
       if (sum(calories) > maxCal) {
-        maxCal = sum(calories)
+        maxCal = sum(calories);
       }
       calories= [];
-      index++;
     }
   }
-  binWidth = width/elves.length
+
+  binWidth = width/elves.length;
+
 }
 
 function draw() {
+  background(255);
+
   for (let i = 0; i < elves.length; i++) {
-    elves[i].display();
+    stroke(0);
+    rect(i*binWidth, 0, binWidth, map(elves[i], 0, maxCal, 0, height))
   }
-  
+
+  if (n < elves.length) {
+    for(let i = 0; i < elves.length - n - i; i++) {
+      let a = elves[i];
+      let b = elves[i+1];
+      if (a < b) {
+        swap(elves, i, i+1);
+      }
+    }
+  } else {
+    console.log("Complete");
+    noLoop();
+  }
+  n++
+}
+
+function swap(arr, a, b) {
+  let tmp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = tmp;
 }
 
 function sum(vals) {
